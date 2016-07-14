4550a24748a6b394c039c82fc3223a67808317a3
aprirez <dd00@bk.ru>
2016-07-14 19:15:42 +0300

    remove some libs

--- project/jni/glshim/src/gl/light.h
+++
@@ -1,6 +0,0 @@
-#include "gl.h"
-
-void glshim_glLightModelf(GLenum pname, GLfloat param);
-void glshim_glLightModelfv(GLenum pname, const GLfloat* params);
-void glshim_glLightfv(GLenum light, GLenum pname, const GLfloat* params);
-void glshim_glLightf(GLenum light, GLenum pname, const GLfloat params);
