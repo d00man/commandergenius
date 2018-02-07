53fe778fc5cc00f43aed2af5964cd900bd6774d1
Andrey Starodubtsev <andrey.starodubtsev@gmail.com>
2018-02-07 20:51:50 +0300

    attempt to make openarena work with modern SDK

--- buildall.sh
+++ buildall.sh
@@ -43,49 +43,47 @@ fi
 # --------------------------------------------------------------------------------
 # Install required android components
 
+declare -a packages_to_install
 add_package() {
-  local package_name=$1
-  if test -z "$packages_to_install"
-  then
-    packages_to_install=$package_name
-  else
-    packages_to_install=$packages_to_install,$package_name
-  fi  
+    local package_name=$1
+    packages_to_install+=("$package_name")
 }
 
 build_tools_version=23.0.3
 min_api_level=9
 max_api_level=23
 
-if ! test -d "$ANDROID_HOME/platform-tools"
-then
+if ! test -d "$ANDROID_HOME/platform-tools"; then
     add_package "platform-tools"
 fi
 
-if ! test -d "$ANDROID_HOME/build-tools/$build_tools_version"
-then
-  add_package "build-tools-$build_tools_version"
+if ! test -d "$ANDROID_HOME/build-tools/$build_tools_version"; then
+    add_package "build-tools;$build_tools_version"
 fi
 
 # For zipalign
 PATH=$ANDROID_HOME/build-tools/$build_tools_version:$PATH
 
-if ! test -d "$ANDROID_HOME/platforms/android-$min_api_level"
-then
-  add_package "android-$min_api_level"
+if ! test -d "$ANDROID_HOME/platforms/android-$min_api_level"; then
+    add_package "platforms;android-$min_api_level"
 fi
 
-if ! test -d "$ANDROID_HOME/platforms/android-$max_api_level"
-then
-  add_package "android-$max_api_level"
+if ! test -d "$ANDROID_HOME/platforms/android-$max_api_level"; then
+    add_package "platforms;android-$max_api_level"
 fi
 
-if test -n "$packages_to_install"
-then
-  echo y | android update sdk --use-sdk-wrapper --no-ui --all --filter "$packages_to_install"
-fi
+install_android_packages() {
+    if test $# -ne 0; then
+        if test -n "$JAVA8_HOME" && test -d "$JAVA8_HOME/bin"; then
+            local PATH="$JAVA8_HOME/bin:$PATH"
+        fi
+        sdkmanager "$@"
+    fi
+}
 
-which android || { echo "'android' tool not found in your PATH" && exit 1 ; }
+install_android_packages "${packages_to_install[@]}"
+
+which sdkmanager || { echo "'android' tool not found in your PATH" && exit 1 ; }
 which zipalign || { echo "'zipalign' tool is not found in your PATH" && exit 1 ; }
 which ant || { echo "'ant' is not found in your PATH" && exit 1 ; }
 which git || { echo "'git' is not found in your PATH" && exit 1 ; }
